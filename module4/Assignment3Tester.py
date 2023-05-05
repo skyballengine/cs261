17240b5770088aebdf42155aecbb7fa9111f3898
Eusebius Ballentine <96203286+skyballengine@users.noreply.github.com>
2023-05-05 13:14:15 -0400

    Added dependencies, added test module and laid out test methods to fill in later

---
+++ module4/Assignment3Tester.py
@@ -0,0 +1,72 @@
+import unittest
+
+
+class Assignment3Tester(unittest.TestCase):
+
+    # Singly Linked List Test Methods
+    def test_insert_front(self):
+        pass
+
+    def test_insert_back(self):
+        pass
+
+    def test_insert_at_index(self):
+        pass
+
+    def test_remove_from_index(self):
+        pass
+
+    def test_remove(self):
+        pass
+
+    def test_count(self):
+        pass
+
+    def test_find(self):
+        pass
+
+    def test_slice(self):
+        pass
+
+    # Stack Dynamic Array
+
+    def test_da_push(self):
+        pass
+
+    def da_pop(self):
+        pass
+
+    def test_da_top(self):
+        pass
+
+    # Stack List Nodes
+
+    def test_ln_push(self):
+        pass
+
+    def test_ln_pop(self):
+        pass
+
+    def test_ln_top(self):
+        pass
+
+    # Queue Dynamic Array
+
+    def test_da_enqueue(self):
+        pass
+
+    def test_da_dequeue(self):
+        pass
+
+    def test_da_front(self):
+        pass
+
+    # Queue List Nodes
+    def test_ln_enqueue(self):
+        pass
+
+    def test_ln_dequeue(self):
+        pass
+
+    def test_ln_front(self):
+        pass
